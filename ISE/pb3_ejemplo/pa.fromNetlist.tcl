88dc2fa2c669ec11d44afc1b0e530728a1ddc320
Leon <fernando.leono@alumno.buap.mx>
2024-01-29 07:32:16 -0600

    commit

--- ISE/pb3_ejemplo/pa.fromNetlist.tcl
+++ ISE/pb3_ejemplo/pa.fromNetlist.tcl
@@ -1,10 +1,10 @@
 
 # PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name pb3_ejemplo -dir "/home/leon/Documents/ISE/pb3_ejemplo/planAhead_run_2" -part xc3s500efg320-4
+create_project -name pb3_ejemplo -dir "/home/leon/Documents/Code/sistemas-empotrados-S6/ISE/pb3_ejemplo/planAhead_run_4" -part xc3s500efg320-4
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "/home/leon/Documents/ISE/pb3_ejemplo/todo.ngc" [ get_property srcset [ current_run ] ]
-add_files -norecurse { {/home/leon/Documents/ISE/pb3_ejemplo} }
+set_property edif_top_file "/home/leon/Documents/Code/sistemas-empotrados-S6/ISE/pb3_ejemplo/todo.ngc" [ get_property srcset [ current_run ] ]
+add_files -norecurse { {/home/leon/Documents/Code/sistemas-empotrados-S6/ISE/pb3_ejemplo} }
 set_param project.pinAheadLayout  yes
 set_property target_constrs_file "todo.ucf" [current_fileset -constrset]
 add_files [list {todo.ucf}] -fileset [get_property constrset [current_run]]

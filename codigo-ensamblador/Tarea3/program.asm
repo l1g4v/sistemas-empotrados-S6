2bd51bfd26fdaeb6619c31dc7674a9b61045b6f0
Leon <fernando.leono@alumno.buap.mx>
2024-02-07 10:58:10 -0600

    codigo

--- codigo-ensamblador/Tarea3/program.asm
+++ codigo-ensamblador/Tarea3/program.asm
@@ -1,1 +1,31 @@
 ;Tarea 3 lcd
+CONSTANT delay_1us_constant, 0B ; at 50Mhz
+;all this region uses registers s0 to s4
+delay_1us:  LOAD s0, delay_1us_constant
+ wait_1us:  SUB s0, 01
+            JUMP NZ, wait_1us
+            RETURN
+
+delay_40us: LOAD s1, 28 ;40 x 1us = 40us
+ wait_40us: CALL delay_1us
+            SUB s1, 01
+            JUMP NZ, wait_40us
+            RETURN
+
+delay_1ms:  LOAD s2, 19 ;25 x 40us = 1ms
+ wait_1ms:  CALL delay_40us
+            SUB s2, 01
+            JUMP NZ, wait_1ms
+            RETURN            
+
+delay_20ms: LOAD s3, 14 ;20 x 1ms = 20ms
+ wait_20ms: CALL delay_1ms
+            SUB s3, 01
+            JUMP NZ, wait_20ms
+            RETURN  
+
+delay_1s:   LOAD s4, 32 ;50 x 20ms = 1000ms
+ wait_1s:   CALL delay_20ms
+            SUB s4, 01
+            JUMP NZ, wait_1s
+            RETURN            
